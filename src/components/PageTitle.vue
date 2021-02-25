e110e28ae8e2f150f1e9cf4c4480d8f0e3ecd32e
Koray Guler <us.koray.guler@gmail.com>
2021-02-25 23:27:54 +0300

    title handler comp

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'vue-title',
+  props: ['title'],
+  watch: {
+    title: {
+      immediate: true,
+      handler() {
+        document.title = this.title;
+      },
+    },
+  },
+  render() {},
+};
+</script>
